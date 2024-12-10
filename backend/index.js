47f1ece32030cef8d18b3587e0cc7f6e9953fa43
sikari009 <asadabbas4337@gmail.com>
2024-12-10 23:34:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 dbConnection();
